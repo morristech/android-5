181e082faddef1d8c71ddb99e61de91748441c83
Naoki Mizuno <nm2372@gmail.com>
2013-05-23 19:39:56 +0900

    Working on the modified version of Addition

--- Addition/src/com/nigorojr/addition/MainActivity.java
+++ Addition/src/com/nigorojr/addition/MainActivity.java
@@ -3,7 +3,6 @@ package com.nigorojr.addition;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
@@ -11,10 +10,12 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-// TODO: Implement View.OnClickListener
 public class MainActivity extends Activity {
+	
+	public static final int REQUEST_ANOTHER_ACTIVITY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +24,13 @@ public class MainActivity extends Activity {
 
         Button button_increment = (Button)findViewById(R.id.button_increment);
         Button button_addition = (Button)findViewById(R.id.button_addition);
+        Button button_for_result = (Button)findViewById(R.id.button_for_result);
         
         ButtonClick button_listener = new ButtonClick();
         
         button_increment.setOnClickListener(button_listener);
         button_addition.setOnClickListener(button_listener);
+        button_for_result.setOnClickListener(button_listener);
     }
 
     @Override
@@ -37,36 +40,70 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    
+    /**
+     * This method is only used when the button "button_result" is clicked.
+     * Sets the TextView in this View to the number that was returned from the other Activity.
+     * Sets to -1 when there is an error.
+     * @param requestCode The request code that was used when starting the other Activity.
+     * @param resultCode The result code that was set in the other activity after setting the result.
+     * @param data The Intent containing the result from the other activity.
+     */
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	Log.i("debug", "req code: " + requestCode);
+    	Log.i("debug", "res code: " + resultCode);
+    	TextView tv = (TextView)findViewById(R.id.result_textView);
+    	//if (requestCode == REQUEST_ANOTHER_ACTIVITY) {
+    		if (resultCode == RESULT_OK)
+	    		tv.setText(data.getStringExtra("ANOTHER_NUMBER"));
+    		else
+    			Toast.makeText(MainActivity.this, "Canceled", Toast.LENGTH_SHORT).show();
+    	//}
+    }
+    
     class ButtonClick implements OnClickListener {
         @Override
         public void onClick(View v) {
+        	// Get the number in the first input field
+            EditText editText = (EditText)findViewById(R.id.input_a);
+            int num_a;
+            if (editText.getText().toString().equals(""))
+            	num_a = 1;
+            else
+                num_a = Integer.parseInt(editText.getText().toString());
+            
+            
             if (v.getId() == R.id.button_increment) {
 
                 // Increment the input number by 1 and change the text inside the text box
-                EditText editText = (EditText)findViewById(R.id.input_a);
-                int incrementedNumber;
-                if (editText.getText().toString().equals(""))
-                	incrementedNumber = 1;
-                else {
-	                incrementedNumber = Integer.parseInt(editText.getText().toString());
-	                incrementedNumber++;
-                }
-                editText.setText(String.valueOf(incrementedNumber));
+                num_a++;
+                editText.setText(String.valueOf(num_a));
                 
 	            // For now, just show a Toast (there are more than 1 way to do it. And don't forget the .show() at the end!)
                 //Toast.makeText(MainActivity.this, "Incremented result is " + incrementedNumber, Toast.LENGTH_LONG).show();
-	            Toast.makeText(v.getContext(), "Incremented result is " + incrementedNumber, Toast.LENGTH_SHORT).show();
+	            Toast.makeText(v.getContext(), "Incremented result is " + num_a, Toast.LENGTH_SHORT).show();
             }
             else if (v.getId() == R.id.button_addition) {
-            	String num_a_String = ((EditText)findViewById(R.id.input_a)).getText().toString();
             	String num_b_String = ((EditText)findViewById(R.id.input_b)).getText().toString();
-            	// Don't do anything if either field is empty
-            	if (num_a_String.equals("") || num_b_String.equals(""))
+            	// Don't do anything if the field is empty
+            	if (num_b_String.equals(""))
             		return;
-	        	int num_a = Integer.parseInt(num_a_String);
 	        	int num_b = Integer.parseInt(num_b_String);
             	Toast.makeText(v.getContext(), (num_a + num_b) + "", Toast.LENGTH_SHORT).show();
+            	
+            	Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            	intent.putExtra("PASSED_STRING", String.valueOf(num_a + num_b));
+            	
+            	startActivity(intent);
+            }
+            
+            else if (v.getId() == R.id.button_for_result) {
+            	Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            	intent.putExtra("FOR_RESULT", String.valueOf(num_a));
+            	Log.i("start", "preparing to start activity for result");
+            	startActivityForResult(intent, REQUEST_ANOTHER_ACTIVITY);
             }
         }
     }
---
+++ Addition/src/com/nigorojr/addition/SecondActivity.java
@@ -0,0 +1,65 @@
+package com.nigorojr.addition;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity implements OnClickListener {
+	
+	private int passed_int;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+		
+		// Get the passed String
+		Intent intent = getIntent();
+		String passed = intent.getStringExtra("PASSED_STRING");
+		
+		// Only set the String if the key is correct, in other words, the "button_for_result" button was clicked
+		if (passed != null) {
+			// Show what has been passed from the previous activity
+			TextView tv = (TextView)findViewById(R.id.text_addition);
+			tv.setText(passed);
+		}
+		else
+			// Probably best if there is a check to see if the getStringExtra returned a non-null String
+			passed_int = Integer.parseInt(intent.getStringExtra("FOR_RESULT"));
+			
+		// Add button that allows the user to close this Activity
+		Button button_close = (Button)findViewById(R.id.button_close);
+		button_close.setOnClickListener(this);
+		
+		Button button_for_result = (Button)findViewById(R.id.button_result);
+		button_for_result.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// Close Activity when the close button is clicked
+		if (v.getId() == R.id.button_close)
+			finish();
+		// Close Activity AFTER setting result
+		else if (v.getId() == R.id.button_result) {
+			Intent intent = getIntent();
+			EditText input_result = (EditText)findViewById(R.id.input_result);
+			String input = input_result.getText().toString();
+			// Don't do anything if the input field is empty
+			if (input.equals(""))
+				return;
+			
+			// Put an Extra to the intent with its value being the sum of the input field num and passed integer + 100
+			intent.putExtra("ANOTHER_NUMBER", Integer.parseInt(input) + passed_int + 100);
+			// Set the result
+			setResult(RESULT_OK, intent);
+			finish();
+		}
+	}
+}
