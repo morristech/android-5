874ffdd7ba899c54901e50139b5f218edc1ffa4f
Naoki Mizuno <nm2372@gmail.com>
2013-07-19 18:58:30 +0900

    Non-deprecated version. Also with some other AlertDialog.

--- DialogTest/src/com/nigorojr/dialogtest/MainActivity.java
+++ DialogTest/src/com/nigorojr/dialogtest/MainActivity.java
@@ -1,14 +1,20 @@
 package com.nigorojr.dialogtest;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 import android.app.Dialog;
-import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+// import android.app.DialogFragment;
+import android.support.v4.app.DialogFragment;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
+    
+    public static final int ID_FOR_DIALOG = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,29 +28,51 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_show_dialog:
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle("This is the set title");
-//                builder.setMessage("This is the message. OK?");
-//                AlertDialog dialog = builder.create();
-//                dialog.show();
-                showDialog(0);
+                // FragmentManager fm = getFragmentManager();
+                FragmentManager fm = getSupportFragmentManager();
+                new DialogFragmentClass().show(fm, "what's a tag");
                 break;
         }
     }
     
-    @Override
-    public Dialog onCreateDialog(int id) {
-        Dialog dialog = null;
-        switch (id) {
-            case 0:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Here's the title");
-                builder.setMessage("And here's the message");
-                dialog = builder.create();
-                break;
+    private static AlertDialog d2 = null;
+    public static class DialogFragmentClass extends DialogFragment implements DialogInterface.OnClickListener {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Here's the title");
+//            builder.setMessage("And here's the message");
+            
+            // If you want to have buttons on the dialog
+            builder.setPositiveButton("Positive!", this);
+            builder.setNeutralButton("Neutral!", this);
+            builder.setNegativeButton("Negative!", this);
+            String[] items = {"Hello", "world", "this", "is", "an", "array", "!!!"};
+            builder.setItems(items, this);
+
+//            Dialog dialog = builder.create();
+//            return dialog;
+            d2 = builder.create();
+            return d2;
         }
-        return dialog;
         
+        // Called when dialog is tapped
+        @Override
+        public void onClick(DialogInterface dialog, int id) {
+            switch (id) {
+                case AlertDialog.BUTTON_POSITIVE:
+                    Toast.makeText(getActivity(), "Positive button clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+                case AlertDialog.BUTTON_NEUTRAL:
+                    Toast.makeText(getActivity(), "Neutral button clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+                case AlertDialog.BUTTON_NEGATIVE:
+                    Toast.makeText(getActivity(), "Negative button clicked!", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Toast.makeText(getActivity(), d2.getListView().getAdapter().getItem(id).toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
